b0ba385e94f7bb7918fb9e38b107c22670bebdb1
SongaMonga <32817093+SongaMonga@users.noreply.github.com>
2018-05-03 12:07:42 +0100

    update da set neighbours pra ter em conta os obstacles. redefini o equals das Edges

--- src/Graph/Edge.java
+++ src/Graph/Edge.java
@@ -11,4 +11,35 @@ class Edge {
 		this.xy_d = xy_d;
 		c = cost;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((xy_d == null) ? 0 : xy_d.hashCode());
+		result = prime * result + ((xy_o == null) ? 0 : xy_o.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Edge other = (Edge) obj;
+		if (xy_d == null) {
+			if (other.xy_d != null)
+				return false;
+		} else if (!xy_d.equals(other.xy_d))
+			return false;
+		if (xy_o == null) {
+			if (other.xy_o != null)
+				return false;
+		} else if (!xy_o.equals(other.xy_o))
+			return false;
+		return true;
+	}
 }
