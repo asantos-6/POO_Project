b8bdaeff9318895e1e608aaf30f2f148269ad213
SongaMonga <32817093+SongaMonga@users.noreply.github.com>
2018-05-07 00:07:57 +0100

    tenho de tratar de uns warnings de unchecked e verificar se posso fazer o que tou a fazer com as subLists

--- src/Event_grid/Reproduction.java
+++ src/Event_grid/Reproduction.java
@@ -20,8 +20,7 @@ public class Reproduction extends Event {
 	public <E> void processEvent(PriorityQueue<E> z_list, Object Simulation) {
 		Simulation sim = (Simulation) Simulation;
 		Individual father = (Individual) this.z; 
-		@SuppressWarnings("unchecked")
-		PriorityQueue<Individual> i_list = (PriorityQueueExt<Individual>) z_list;
+		PriorityQueueExt<Individual> i_list = (PriorityQueueExt<Individual>) z_list;
 		
 		if(father.isAlive()) {
 		
@@ -30,7 +29,9 @@ public class Reproduction extends Event {
 			int length_child = (int) Math.ceil(0.9*father.getPath().size());
 
 			ArrayList<Coord> child_path = (ArrayList<Coord>) father.getPath().subList(0, length_child);
+			child_path = (ArrayList<Coord>) child_path.clone(); //??
 			ArrayList<Integer> child_cost = (ArrayList<Integer>) father.getCost().subList(0, length_child);
+			child_cost = (ArrayList<Integer>) child_cost.clone();//??
 			
 			Individual child = new Individual(child_path, child_cost, sim.getGrid().getNodes(), sim.getPec(), this.t);
 			i_list.add(child);
